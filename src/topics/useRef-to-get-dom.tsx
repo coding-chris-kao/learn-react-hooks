71eec37c82664d63cfc62417612d40e5e4ae7dc2
coding-chris-kao <chi1010860@gmail.com>
2022-11-02 10:37:30 +0800

    feat: add 3 topics

---
+++ src/topics/useRef-to-get-dom.tsx
@@ -0,0 +1,19 @@
+import { useRef, useEffect, useState } from "react";
+
+export function UseRefToGetDom() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // main part: execute every time the component rerender
+  console.log("render");
+
+  function onSubmit() {
+    console.log({ name: inputRef.current?.value });
+  }
+
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={onSubmit}>Submit</button>
+    </>
+  );
+}
